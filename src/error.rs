a4833ff13fa15adfa1a5a3571ee5353290b21d2d
Nikolay Edigaryev <edigaryev@gmail.com>
2025-01-08 01:11:01 +0400

    VmnetErrorStatus: show the actual status returned by vmnet
    We do this for VmnetUnknownStatus, but not for VmnetErrorStatus
    for some reason.

--- src/error.rs
+++ src/error.rs
@@ -3,7 +3,7 @@ use crate::ffi::vmnet;
 /// A specialized [`Error`](https://doc.rust-lang.org/std/error/trait.Error.html) type for errors involving [vmnet.framework](https://developer.apple.com/documentation/vmnet).
 #[derive(thiserror::Error, Debug)]
 pub enum Error {
-    #[error("vmnet returned an error status")]
+    #[error("vmnet returned an error status: {0:?}")]
     VmnetErrorStatus(vmnet::Status),
     #[error("vmnet returned an unknown status: {0}")]
     VmnetUnknownStatus(u32),
