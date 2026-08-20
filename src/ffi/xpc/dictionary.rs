22926101de476aed3bd6adc7b10ac88cd97ef9a1
Nikolay Edigaryev <edigaryev@gmail.com>
2026-08-20 10:07:22 +0100

    Move CBool wrapper to module scope for clarity

--- src/ffi/xpc/dictionary.rs
+++ src/ffi/xpc/dictionary.rs
@@ -11,6 +11,26 @@ use std::ffi::{CStr, CString};
 use std::os::raw::c_char;
 use std::rc::Rc;
 
+#[repr(transparent)]
+#[derive(Clone, Copy)]
+struct CBool(bool);
+
+impl CBool {
+    const TRUE: Self = Self(true);
+    const FALSE: Self = Self(false);
+}
+
+// SAFETY: `xpc_dictionary_applier_t` returns C `bool`[1], which preprocesses
+// to C99 `_Bool`[2]; objc2 recommends using a transparent wrapper around
+// `bool` in this case[3].
+//
+// [1]: grep xpc_dictionary_applier_t "$(xcrun --show-sdk-path)/usr/include/xpc/xpc.h"
+// [2]: grep '#define bool' "$(xcrun clang -print-resource-dir)/include/stdbool.h"
+// [3]: https://docs.rs/objc2/latest/objc2/runtime/struct.Bool.html
+unsafe impl Encode for CBool {
+    const ENCODING: Encoding = Encoding::Bool;
+}
+
 pub struct Dictionary {
     xdict: XpcObjectT,
 }
@@ -100,26 +120,6 @@ impl TryFrom<Dictionary> for HashMap<String, XpcData> {
     fn try_from(value: Dictionary) -> Result<Self, Self::Error> {
         let result = Rc::new(RefCell::new(HashMap::new()));
         let result_weak = Rc::downgrade(&result);
-
-        #[repr(transparent)]
-        #[derive(Clone, Copy)]
-        struct CBool(bool);
-
-        impl CBool {
-            const TRUE: Self = Self(true);
-            const FALSE: Self = Self(false);
-        }
-
-        // SAFETY: `xpc_dictionary_applier_t` returns C `bool`[1], which preprocesses
-        // to C99 `_Bool`[2]; objc2 recommends using a transparent wrapper around
-        // `bool` in this case[3].
-        //
-        // [1]: grep xpc_dictionary_applier_t "$(xcrun --show-sdk-path)/usr/include/xpc/xpc.h"
-        // [2]: grep '#define bool' "$(xcrun clang -print-resource-dir)/include/stdbool.h"
-        // [3]: https://docs.rs/objc2/latest/objc2/runtime/struct.Bool.html
-        unsafe impl Encode for CBool {
-            const ENCODING: Encoding = Encoding::Bool;
-        }
 
         let block = RcBlock::new(move |key: *const c_char, value: XpcObjectT| -> CBool {
             let key = unsafe { CStr::from_ptr(key).to_string_lossy().to_string() };
